8ff75e34a91876df9fbf3fe506ba8fd0b2e14f0b
Hebert <hebertphp@gmail.com>
2022-09-05 14:35:08 -0300

    PBE_3a_M

---
+++ aula3/aula3_01-1.php
@@ -0,0 +1,10 @@
+<?php
+$dia = 30;
+$mes = "Agosto";
+$ano = 2022;
+$junta=$dia.$ano;
+$anocompleto = $dia . " de " . $mes . " de " . $ano;//concatenar
+echo "Data de hoje: " . $anocompleto."<br>";//concatenar
+echo "São Paulo, $dia de $mes de $ano<hr>";
+var_dump($dia.$mes);
+?>
---
+++ aula3/aula3_02-1.php
@@ -0,0 +1,11 @@
+<?php
+$x = 06437010; 
+$y = -123; 
+echo "x = $x <br>";
+echo "y = $y <br>";
+var_dump($x);
+/*
+10      decimal (0-9)
+010     octal (0-7)
+*/
+?>
