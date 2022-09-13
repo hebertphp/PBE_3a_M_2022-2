07545bf03ebe7a349f98ceffd21ae19a159e732e
Hebert <hebertphp@gmail.com>
2022-09-13 08:39:42 -0300

    PBE_3a_M

---
+++ aula3/aula3_07-1.php
@@ -0,0 +1,7 @@
+<?php
+$nota = 9.5;//9.5 float
+$nova_nota = (int)$nota;//9 int
+var_dump($nota);
+echo "<br>";
+var_dump($nova_nota);
+?>
---
+++ aula3/aula3_08-1.php
@@ -0,0 +1,6 @@
+<?php
+define("PI", 3.14);
+define("HOST","localhost");
+$valor = 2 * PI;//2 * 3.14 = 6.28
+echo HOST . "  $valor";
+?>
