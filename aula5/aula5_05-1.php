e73ffe038ee457330ffd25b0ca19f71db2c3e306
Hebert <hebertphp@gmail.com>
2022-10-18 10:39:18 -0300

    18/10/22

--- aula5/aula5_05-1.php
+++ aula5/aula5_05-1.php
@@ -5,3 +5,6 @@ Tranformar o código switch ($destino) para if elseif else
 
 
 ?>
+<!-- 
+<img src="https://raw.githubusercontent.com/bgeneto/bandeiras-br/master/imagens/SP.png" width="100"> 
+-->
---
+++ aula5/aula5_06.php
@@ -0,0 +1,23 @@
+<?php
+$i=0; //VI
+while ($i <= 5) {//cond
+    echo $i . " ";
+    $i++;//cont
+}
+/*
+    $i  ($i<=5) Saída   $i++
+    0   V       0       1
+    1   V       1       2
+    2   V       2       3
+    3   V       3       4
+    4   V       4       5
+    5   v       5       6
+    6   F
+*/
+?>
+<br>==========<br>
+Mostar os valores 7 8 9 10<br>
+<?php
+
+
+?>
