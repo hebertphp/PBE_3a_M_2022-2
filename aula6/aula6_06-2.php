d07afb7208af9c3a38cfd363392244355b160c04
Hebert <hebertphp@gmail.com>
2022-11-29 09:42:28 -0300

    29/11/22

--- aula6/aula6_06-2.php
+++ aula6/aula6_06-2.php
@@ -4,6 +4,13 @@ $cliente=array(
     "nome"=>"Abelerda Pafuncia",
     "data"=>"28/11/2022"
 );
-
+//foreach($var_tipo_array as $var_pos=>$var_valor)
+foreach($cliente as $pos=>$vxv){
+    echo "$pos: $vxv <br>";
+}
 // CPF: NNN.NNN.NN8-NN
+/*
+cpf: 111.111.118-11
+nome: Abelerda Pafuncia
+*/
 ?>
