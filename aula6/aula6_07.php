2e998116c6f5491893e501fc254fe1e4a92e92fd
Hebert <hebertphp@gmail.com>
2022-11-29 10:01:56 -0300

    29/11/22

--- aula6/aula6_07.php
+++ aula6/aula6_07.php
@@ -1,12 +1,18 @@
 <?php
-    $nomes=array("Fulano","Beltrano","Sicrano", "Astrogildo");
-    $total = count($nomes);
-    $ult = count($nomes)-1;
+    //              0       1          2          3            4
+    $nomes=array("Fulano","Beltrano","Sicrano", "Astrogildo","Bete");
+    $total = count($nomes);//5
+    $ult = count($nomes)-1;//4
     echo "Foram encontradas $total nomes no array.<br>";
     echo "Primeiro valor do array: ".$nomes[0]."<br>";
-    echo "Último valor do array: ".$nomes[$ult];
+    echo "Último valor do array: ".$nomes[count($nomes)-1];//Bete
     echo "<hr>";
-    for($i=0;$i<$total;$i++){
+    for($i=0;$i<count($nomes);$i++){
         echo $nomes[$i]."<br>";
     }
+    /*
+    se tiver um array com 100 valores, posições numéricas, 
+    qual a posição do primeiro e do último valor
+    primeiro [0], último [99]
+    */
 ?>
