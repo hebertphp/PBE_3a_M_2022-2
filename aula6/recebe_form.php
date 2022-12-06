b4d40d2575ec6cd0578bcf3a0d192388b89bf9a0
Hebert <hebertphp@gmail.com>
2022-12-06 11:27:20 -0300

    06/12/2022

--- aula6/recebe_form.php
+++ aula6/recebe_form.php
@@ -1,5 +1,9 @@
 <?php
-
+if(count($_POST)==0){
+    echo "ERRO!";
+    echo "<a href='aula6_10.php'>Clique aqui para o Formulário</a>";
+    exit();
+}
 $uf=$_POST["uf"];
 echo "<pre>";print_r($_POST);echo "</pre>";
 echo "UF selecionado: $uf";
