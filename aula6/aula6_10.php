42b26a2cf421b96c1f5a1c56f4d5684e97f68fd4
Hebert <hebertphp@gmail.com>
2022-12-06 10:39:16 -0300

    06/12/2022

---
+++ aula6/aula6_10.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+    <form action="#">
+
+    Selecione UF
+    <select name="uf" id="uf">
+        <option value="SP">SP</option>
+        <option value="RJ">RJ</option>
+        <option value="MG">MG</option>
+        <option value="ES">ES</option>
+    </select>    
+    </form>
+</body>
+</html>
